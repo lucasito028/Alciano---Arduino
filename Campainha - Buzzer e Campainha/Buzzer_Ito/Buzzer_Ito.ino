16858e9183877dc52fe3c03f94d8727080101f50
lucasito028 <lucastoshihiroito28@gmail.com>
2024-04-02 17:43:02 -0300

    Arduino

---
+++ Campainha - Buzzer e Campainha/Buzzer_Ito/Buzzer_Ito.ino
@@ -0,0 +1,32 @@
+int buzzer = 1;
+int btn = 12;
+int led = 1;
+int led2 = 1;
+int valor = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(buzzer, OUTPUT);
+  pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(btn, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if(valor == 1){
+    valor = digitalRead(btn);
+    tone(1, 40, 3000);
+    delay(200);
+    tone(1, 200, 100);
+    delay(200);
+    digitalWrite(led, OUTPUT);
+  }else{
+      valor = digitalRead(btn);
+
+  }
+
+
+}

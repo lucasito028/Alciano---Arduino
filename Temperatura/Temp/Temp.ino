e1b840e38ad803a0d464500328756913c5344f50
Lucas T Ito <89547865+lucasito028@users.noreply.github.com>
2024-04-22 14:50:50 -0300

    Update Temp.ino

--- Temperatura/Temp/Temp.ino
+++ Temperatura/Temp/Temp.ino
@@ -1,22 +1,42 @@
-//Declarando Valores
-float sensortemp = A0;
-float valor = 0;
-float milivolts = 0;
-float temp = 0;
+/*
+  Este código lê o valor de um sensor de temperatura conectado ao pino A0 e imprime a temperatura em milivolts no Monitor Serial.
 
-//Agora o Setup
-void setup(){
-    pinMode(sensortemp, INPUT);
-    Serial.begin(9600);
+  Conexões:
+  - O sensor de temperatura está conectado ao pino A0.
+
+  O código lê o valor do sensor de temperatura, converte-o em milivolts e imprime o resultado no Monitor Serial a cada segundo.
+
+  Autor: [Seu Nome/Autor]
+  Data de Criação: [Data de Criação]
+*/
+
+// Declarando as variáveis
+float sensortemp = A0;   // Pino do sensor de temperatura
+float valor = 0;         // Variável para armazenar o valor lido do sensor
+float milivolts = 0;     // Variável para armazenar a temperatura em milivolts
+float temp = 0;          // Variável para armazenar a temperatura em graus Celsius
+
+void setup() {
+  // Configurando o pino do sensor de temperatura como entrada
+  pinMode(sensortemp, INPUT);
+  
+  // Iniciando a comunicação serial
+  Serial.begin(9600);
 }
 
-//Cicle
-void loop(){
-
-    valor = analogRead(sensortemp);
-    milivolts = (valor * (500.0 / 1023.0) );
-    temp = milivolts;
-    delay(1000);
-    Serial.println(temp);
-
+void loop() {
+  // Lendo o valor do sensor de temperatura
+  valor = analogRead(sensortemp);
+  
+  // Convertendo o valor lido em milivolts
+  milivolts = (valor * (500.0 / 1023.0));
+  
+  // Atribuindo o valor de milivolts à variável de temperatura
+  temp = milivolts;
+  
+  // Imprimindo a temperatura em milivolts no Monitor Serial
+  Serial.println(temp);
+  
+  // Aguardando 1 segundo
+  delay(1000);
 }

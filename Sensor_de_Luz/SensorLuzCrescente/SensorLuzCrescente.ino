437a5f4cfeacd41d500bb5585bf516dc1759bc82
lucasito028 <lucastoshihiroito28@gmail.com>
2024-05-07 17:54:51 -0300

    Melhorias

--- Sensor_de_Luz/SensorLuzCrescente/SensorLuzCrescente.ino
+++ Sensor_de_Luz/SensorLuzCrescente/SensorLuzCrescente.ino
@@ -1,12 +1,20 @@
 // Declaração das variáveis
 float sensorluz = A0;  // Porta analógica à qual o sensor de luz está conectado
-int led = 5;            // Porta digital à qual o LED está conectado
+int led1 = 6;            // Porta digital à qual o LED está conectado
+int led2 = 2;
+int led3 = 3; 
+int led4 = 4; 
+int led5 = 5; 
 float luminosidade = 0; // Variável para armazenar o valor da luminosidade
 
 void setup() {
   // Configuração inicial
   pinMode(sensorluz, INPUT); // Define a porta do sensor de luz como entrada
-  pinMode(led, OUTPUT);       // Define a porta do LED como saída
+  pinMode(led1, OUTPUT); 
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);      // Define a porta do LED como saída
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
   
   // Inicialização da comunicação serial
   Serial.begin(9600); // Inicia a comunicação serial com uma taxa de 9600 bauds
@@ -24,11 +32,17 @@ void loop() {
   delay(500);
 
   // Verifica se a luminosidade é menor ou igual a 500
-  if (luminosidade <= 500) {
-    digitalWrite(led, HIGH); // Acende o LED se a luminosidade for menor ou igual a 500
+  
+  if (luminosidade <= 350) {
+    digitalWrite(led1, 1);
+
   } else {
-    digitalWrite(led, LOW);  // Desliga o LED se a luminosidade for maior que 500
+    digitalWrite(led1, 0);
   }
-}
+
+  if ((luminosidade < 300) && (luminosidade > 270)) {
+    
+  }
+
 
 }
